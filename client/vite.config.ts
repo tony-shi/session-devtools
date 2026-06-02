34ecba9f32bf7244634f91d1426bd476f600c079
tony-shi <tony-shi@users.noreply.github.com>
2026-06-02 12:54:48 +0800

    feat: fix dev port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: CLIENT_PORT,
+      strictPort: true,
       open: true,
       proxy: {
         "/api": `http://localhost:${SERVER_PORT}`,
